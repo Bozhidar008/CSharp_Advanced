f447341cc6568b8e7f66a601ef8a7b58d7791410
Bozhidar Marinov <Bozhidar.marinov.a35@learn.telerikacademy.com>
2022-05-19 11:48:26 +0300

    Add MaxAndMinElement Solution

---
+++ MaxAndMinElement/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxAndMinElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                string command = input[0];
+
+                switch (command)
+                {
+                    case "1":
+                        int number = int.Parse(input[1]);
+                        stack.Push(number); break;
+                    case "2": stack.Pop(); break;
+                    case "3":
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            var array = stack.ToArray();
+                            Console.WriteLine(array.Max()); break;
+                        }
+
+                    case "4":
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            var array1 = stack.ToArray();
+                            Console.WriteLine(array1.Min()); break;
+                        }
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(String.Join(", ", stack));
+        }
+    }
+}
