ea3ae9eae2dc146926710020697826a013b3fab4
Bozhidar Marinov <Bozhidar.marinov.a35@learn.telerikacademy.com>
2022-05-19 10:49:59 +0300

    Add Solution ReverseString

---
+++ ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (var ch in str)
+            {
+                stack.Push(ch);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
