ec0099449142e18a33293a5fbaf97299c42bff50
Bozhidar Marinov <Bozhidar.marinov.a35@learn.telerikacademy.com>
2022-05-21 14:14:14 +0300

    Add 1.SumMatrixElements

--- 1. Sum Matrix Elements/Program.cs
+++ 1. Sum Matrix Elements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _1._Sum_Matrix_Elements
 {
@@ -6,7 +7,27 @@ namespace _1._Sum_Matrix_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] param = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = param[0];
+            int cols = param[1];
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int sum = 0;
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
         }
     }
 }
