816a080b587fa79e33b3657dc22867cbcdcceb10
Bozhidar Marinov <Bozhidar.marinov.a35@learn.telerikacademy.com>
2022-05-21 12:44:43 +0300

    Add FashinBoutiqueAdd

---
+++ BasicStackOperations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicStackOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] n = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int numberToAdd = int.Parse(n[0]);
+            int numberToRemove = int.Parse(n[1]);
+            int numberToCheck = int.Parse(n[2]);
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+
+            for (int i = 0; i < numberToRemove; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Contains(numberToCheck))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine("0");
+                }
+                else
+                {
+                    var array = stack.ToArray();
+                    Console.WriteLine(array.Min());
+                }
+
+            }
+
+        }
+    }
+}
---
+++ FashionBoutique/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>(array);
+            int sum = 0;
+            int counter = 1;
+            while (stack.Count > 0)
+            {
+                if ((sum + stack.Peek()) <= capacity)
+                {
+                    sum += stack.Pop();
+                }
+                if (stack.Count == 0)
+                {
+                    break;
+                }
+                if ((sum + stack.Peek()) > capacity)
+                {
+                    counter++;
+                    sum = stack.Pop();
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
