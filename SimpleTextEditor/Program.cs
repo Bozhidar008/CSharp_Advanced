ad12b423bfbf52615488e5b855d5a528a2dcdfaf
Bozhidar Marinov <Bozhidar.marinov.a35@learn.telerikacademy.com>
2022-05-21 13:19:39 +0300

    add SimpleTextEditor

---
+++ SimpleTextEditor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<string> stack = new Stack<string>();
+            string text = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                string command = input[0];
+                switch (command)
+                {
+                    case "1":
+                        stack.Push(text);
+                        text += input[1];
+                        break;
+                    case "2":
+                        stack.Push(text);
+                        text = text.Substring(0, text.Length - int.Parse(input[1]));
+                        break;
+                    case "3":
+                        Console.WriteLine(text[int.Parse(input[1]) - 1]);
+                        break;
+                    case "4":
+                        text = stack.Pop();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
