4f78e1b23f7c412dc852a64a530ce4bc59a59148
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-29 12:48:41 +0300

    Add Solution CitiesByContinentAndCountry

---
+++ Cities by Continent and Country/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> dic = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+                if (!dic.ContainsKey(continent))
+                {
+                    dic.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!dic[continent].ContainsKey(country))
+                {
+                    dic[continent][country] = new List<string>();
+                }
+                dic[continent][country].Add(city);
+            }
+            
+            foreach (var (continent, country) in dic)
+            {
+                Console.WriteLine($"{continent}:");
+                foreach (var (countryName, city) in country)
+                {
+                    
+                    Console.WriteLine($" {countryName} -> {string.Join(", ", city)}");
+                }
+            }
+        }
+    }
+}
