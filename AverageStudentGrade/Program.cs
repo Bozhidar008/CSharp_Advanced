18d8e972cd0605e1835ad08286a01445daa937e0
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-29 11:31:00 +0300

    Add Solution AverageGrades

---
+++ AverageStudentGrade/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> map = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var key = input[0];
+                var value = double.Parse(input[1]);
+                if (!map.ContainsKey(key))
+                {         
+                    map.Add(key, new List<decimal> { });
+                    
+                }                
+                    map[key].Add((decimal)value);                
+            }            
+            foreach (var(key,value) in map)
+            {
+                string AllValues = string.Join(" ", value.Select(x => x.ToString("F2")));
+                Console.WriteLine($"{key} -> {AllValues} (avg: {value.Average():f2})");
+            }
+        }
+    }
+}
