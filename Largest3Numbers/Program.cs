18622ec06e9aa14f4b23cf672ea4b47549fec184
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-29 10:53:34 +0300

    Add Solution Largest3NUmber

---
+++ Largest3Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            nums.OrderByDescending(x=>x).Take(3).ToList().ForEach(x => Console.Write(x+ " "));
+        }
+    }
+}
