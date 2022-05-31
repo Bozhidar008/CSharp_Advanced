d30f9de1741a96d7c0053de770da45f4cf43f38b
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-31 11:30:20 +0300

    Add Solution 03. Periodic Table

---
+++ 03. Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                                
+                    set.UnionWith(input);
+            }
+
+            foreach (var item in set.OrderBy(s=>s))
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
