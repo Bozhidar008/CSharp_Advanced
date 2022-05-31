d9189485380bbb4bc7ea7136a9af65c708a76615
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-31 10:07:57 +0300

    Add Solution SetsOfElements

---
+++ 02. Sets of Elements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int n = numbers[0];
+            int m = numbers[1];
+            HashSet<int>setN = new HashSet<int>();
+            HashSet<int>setM = new HashSet<int>();
+            
+            for (int i = 0; i < n+m; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (i < n)
+                {
+                    setN.Add(input);
+                }
+                if (i >= m)
+                {
+                     setM.Add(input);
+                }
+            }
+             setN.IntersectWith(setM);
+            foreach (var item in setN)
+            {
+                Console.Write(item+ " ");
+            }  
+
+            
+        }
+    }
+}
