4dcb5fab75db4b7084062a73e9bc39be9e0e3b45
Bozhidar Marinov <bojidar008@gmail.com>
2022-05-31 12:14:44 +0300

    Add Solution 04. Even Times

---
+++ 04. Even Times/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           
+            Dictionary<int, int> set = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (!set.ContainsKey(num))
+                {
+                    set[num] = 0;
+                }
+                set[num]++;
+            }
+            foreach (var num in set)
+            {
+                if (num.Value % 2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+                }
+            }            
+        }
+    }
+}
