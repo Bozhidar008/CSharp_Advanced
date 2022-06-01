0f4fd5a5d55b0b4b14436f09fa857f7a3ed47ba7
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-01 09:04:23 +0300

    Add Solution 05. Count Symbols

---
+++ 05. Count Symbols/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+            SortedDictionary<char, int> map = new SortedDictionary<char, int>();
+
+            foreach (var item in text)
+            {
+                if (!map.ContainsKey(item))
+                {
+                    map.Add(item, 0);
+                }
+                map[item]++;
+            }
+            foreach (var item in map)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
