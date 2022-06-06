5c40d4f4c107ad7a412f56c6cc86a05013dbfcfc
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-06 20:26:53 +0300

    Add Solution ineNumbers

---
+++ LineNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputPath, string outputPath)
+        {
+
+            int lineNum = 1;
+            StreamReader reader = new StreamReader(inputPath);
+            using (reader)
+            {
+                StreamWriter writer = new StreamWriter(outputPath);
+                using (writer)
+                {
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        
+                        
+                        writer.WriteLine($"{lineNum}. {line}");
+                        
+                        lineNum++;
+                    }
+                }
+            }
+
+        }
+    }
+}
