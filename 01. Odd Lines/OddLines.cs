5934bb20a0da20ed0436634e1d6aefc7f73d6153
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-06 14:57:31 +0300

    Add Solution OddLines

---
+++ 01. Odd Lines/OddLines.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    internal class OddLines
+    {
+
+
+        static void Main()
+        {
+            string inputFilePath = @"C:\GitHub\Bozhidar branch\01. Odd Lines\bin\Debug\net5.0\input.txt";
+            string outputFilePath = @"C:\GitHub\Bozhidar branch\01. Odd Lines\bin\Debug\net5.0\output.txt";
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            int lineNum = 0;
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                StreamWriter writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        if (lineNum % 2 != 0)
+                        {
+                            writer.WriteLine(line);                            
+                        }
+                        lineNum++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OddLines/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+
+            int lineNum = 0;
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                StreamWriter writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        if (lineNum % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        lineNum++;
+                    }
+                }
+            }
+
+        }
+    }
+}
