7f14de5a05b996e3401a6051d99f6890fc01feff
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-07 13:54:19 +0300

    Add Solution DirectoryTraversal

---
+++ CopyDirectory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CopyDirectory
+{
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
