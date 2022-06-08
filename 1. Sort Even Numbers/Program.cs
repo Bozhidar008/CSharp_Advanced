43b1bd8a931c609f747c09c4bb651a4591688035
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-08 12:21:41 +0300

    Add Solution SortEvenNumbers

---
+++ 1. Sort Even Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+           var result = input.Where(n => n % 2 == 0).OrderBy(n=>n);
+            Console.WriteLine(String.Join(", ", result));
+        }
+    }
+}
---
+++ ZipAndExtract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipAndExtract
+{
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"..\..\..\Files\copyMe.png";
+            string zipArchiveFile = @"..\..\..\Files\archive.zip";
+            string extractedFile = @"..\..\..\Files\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFile, string zipArchiveFile)
+        {
+            ZipFile.CreateFromDirectory(inputFile, zipArchiveFile);
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFile, string fileNameOnly, string extractedFile)
+        {
+            ZipFile.ExtractToDirectory(fileNameOnly, zipArchiveFile,  true);
+        }
+    }
+}
