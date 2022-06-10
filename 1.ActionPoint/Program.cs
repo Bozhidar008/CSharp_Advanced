490e585ccb53377da28ca24fd7dc2a4031165a00
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-10 18:47:22 +0300

    Add Solution 1.ActionPoint

---
+++ 1.ActionPoint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.ActionPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            //foreach (var item in input)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Action<string> print = word => Console.WriteLine(word);
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(print);    
+        }
+    }
+}
