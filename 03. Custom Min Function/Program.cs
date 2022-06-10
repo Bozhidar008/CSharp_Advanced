31b4b80fe52564897c80b69d906c3ab6c3da5c8d
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-10 19:03:24 +0300

    Add Solution 03. Custom Min Function

---
+++ 03. Custom Min Function/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> func = GetMin;
+            Console.WriteLine(func(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray()));
+                
+        }
+        private static int GetMin(int[] array)
+        {
+            int minNum = int.MaxValue;
+
+            foreach (var item in array)
+            {
+                if (item<minNum)
+                {
+                    minNum = item;
+                }
+            }
+            return minNum;
+        }
+    }
+}
