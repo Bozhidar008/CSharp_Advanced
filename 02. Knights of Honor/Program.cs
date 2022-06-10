612370d5cbea7d2d329a84daeecdc9788918e9d6
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-10 18:52:47 +0300

    Add Solution 02._Knights_of_Honor

---
+++ 02. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> action = word => Console.WriteLine("Sir"+" "+word);
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(action);
+        }
+    }
+}
