5eb790ffb0ccc815a29f18b7ee46d3f525d655b7
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-12 16:41:56 +0300

    Add Solution 07. Predicate For Names

---
+++ 07. Predicate For Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Func<string[], string[]> length = num => num.Where(x => x.Length <= n).ToArray();
+
+            foreach (var item in length(names))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
