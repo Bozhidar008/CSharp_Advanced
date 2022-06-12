5d2a961b3a463885cdd3ec0211de7cf7c6d663c2
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-12 16:29:13 +0300

    Add Solution 06. Reverse And Exclude

---
+++ 06. Reverse And Exclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray();
+            int num = int.Parse(Console.ReadLine());
+            
+            Func<int[], int[]> divisible = n => n.Where(x => x % num !=0).ToArray();
+            Console.WriteLine(String.Join(" ", divisible(array)));
+        }
+    }
+}
