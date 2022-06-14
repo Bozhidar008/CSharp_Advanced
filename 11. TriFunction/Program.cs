36a4831e3965684eb03b61ab76e12d46415ba89d
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-14 18:48:26 +0300

    Add Solution 11. TriFunction

---
+++ 11. TriFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetASCII = int.Parse(Console.ReadLine());
+            string[] inputNames = Console.ReadLine().Split();
+            Action<string> Print = name => Console.WriteLine(name);
+            Func<string, int, bool> IsSumEqual =
+                        (name, ascii) => name.Sum(c => c) >= ascii;
+            Func<string[], Func<string, int, bool>, string> FindFirstName =
+                        (names, IsSumEqualASCII) => names.FirstOrDefault(n => IsSumEqual(n, targetASCII));
+            string result = FindFirstName(inputNames, IsSumEqual);
+            Console.WriteLine(result);
+        }
+    }
+}
