8c71e064d6ebf97356b6e7d366c5bfbbbbe5297e
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-17 10:38:24 +0300

    add Solution 02. Car Extension

---
+++ 02. Car Extension/StartUp.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+
+            
+        }
+    }
+
+
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                this.year = value;
+            }
+        }
+        public double FuelQuantity
+        {
+            get
+            {
+                return this.fuelQuantity;
+            }
+            set
+            {
+                this.fuelQuantity = value;
+            }
+        }
+        public double FuelConsumption
+        {
+            get
+            {
+                return this.fuelConsumption;
+            }
+            set
+            {
+                this.fuelConsumption = value;
+            }
+        }
+
+        public void Drive(double distance)
+        {
+            double result = fuelQuantity - distance * fuelConsumption;
+            if (result > 0)
+            {
+                fuelQuantity = result;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+           return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.fuelQuantity:f2}";
+        }
+    }
+}
