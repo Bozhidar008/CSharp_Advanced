3ba7a32a3f04f8a2689f6a22d2a7e56ea14f669e
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-17 19:36:00 +0300

    Add Solution 04. Opinion Poll

---
+++ OpinionPoll/Family.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+
+public class Family
+{
+    private List<Person> persons;
+
+    public Family()
+    {
+        this.persons = new List<Person>();
+    }
+
+    public void AddMember(Person member)
+    {
+        persons.Add(member);
+    }
+
+    public Person GetOldestMember()
+    {
+        return persons.OrderByDescending(p => p.Age).First();
+
+    }
+
+    public List<Person> GetPollMembers()
+    {
+        List<Person> pollMembers = this.persons.Where(p => p.Age > 30).OrderBy(f=>f.Name).ToList();
+        return pollMembers;
+    }
+}
+
---
+++ OpinionPoll/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Family people = new Family();
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = input[0];
+            int age = int.Parse(input[1]);
+            Person person = new Person(name, age);
+            people.AddMember(person);
+        }
+
+        var newfamily = people.GetPollMembers();
+
+        foreach (var item in newfamily)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
+
