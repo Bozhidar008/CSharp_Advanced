99675a6113ab836f22193c784b66b19cc1f8ff6a
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-17 10:21:21 +0300

    Add Solution 01.Car

---
+++ 01. Car/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _01._Car
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        public string Make
+        {
+            get
+            {
+                return this.make;
+            }
+            set
+            {
+                this.make = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return this.year;
+            }
+            set
+            {
+                this.year = value;
+            }
+        }
+    }
+}
