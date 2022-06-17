e0fdac19e75d766e335c103565f71a972d52c896
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-17 19:18:59 +0300

    Add Solution 03. Oldest Family Member

---
+++ 03. Oldest Family Member/Family.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+
+public class Family
+{
+    private List<Person> persons;
+
+    public Family()
+    {
+        this.persons = new List<Person>();
+    }
+
+    public void AddMember(Person member)
+    {
+        persons.Add(member);
+    }
+
+    public Person GetOldestMember()
+    {
+        return persons.OrderByDescending(p => p.Age).First();
+
+    }
+}
+
