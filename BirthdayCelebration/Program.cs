043c435868e3fe6e3536a00fb32d5199a0485772
Bozhidar Marinov <bojidar008@gmail.com>
2022-06-24 21:15:28 +0300

    Add Solution BirthdayCelebration

---
+++ BirthdayCelebration/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Birthday_Celebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var guests = new LinkedList<int>(Console
+                 .ReadLine()
+                 .Split()
+                 .Select(int.Parse));
+
+            var plates = new Stack<int>(Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            var wastedGramsOfFood = 0;
+
+            while (guests.Any() && plates.Any())
+            {
+                var currentGuest = guests.First();
+                var currentPlate = plates.Peek();
+
+                if (currentGuest < currentPlate)
+                {
+                    wastedGramsOfFood += currentPlate - currentGuest;
+                    guests.RemoveFirst();
+                    plates.Pop();
+                }
+                else if (currentGuest > currentPlate)
+                {
+                    var stillHungry = currentGuest - currentPlate;
+                    plates.Pop();
+                    guests.RemoveFirst();
+                    guests.AddFirst(stillHungry);
+                }
+                else
+                {
+                    plates.Pop();
+                    guests.RemoveFirst();
+                }
+            }
+
+            Console.WriteLine(plates.Any() ? $"Plates: {string.Join(" ", plates)}" : $"Guests: {string.Join(" ", guests)}");
+            Console.WriteLine($"Wasted grams of food: {wastedGramsOfFood}");
+        }
+    }
+}
